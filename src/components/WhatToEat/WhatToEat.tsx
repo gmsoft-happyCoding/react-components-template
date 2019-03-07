109665c493cca6acd94755136df7a145178f5dc9
angular-moon <xiaoxuss@163.com>
2019-03-07 14:08:42 +0800

    improve demo

--- src/components/WhatToEat/WhatToEat.tsx
+++ src/components/WhatToEat/WhatToEat.tsx
@@ -1,13 +1,13 @@
+import React, { useState, useEffect } from 'react';
 import { Button, Card, Input, Switch } from 'antd';
-import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { ActionCreatorsMapObject } from 'redux';
 import styled from 'styled-components';
-import { Mode } from '../../enums/Mode';
-import whatToEatActions from '../../models/whatToEat/whatToEat.action';
-import whatToEatMode from '../../models/whatToEat/whatToEat.model';
-import { Food } from '../../types/Food.d';
-import { bindActions, stateContainer } from '../../utils';
+import { Mode } from '@/enums/Mode';
+import { stateContainer, bindActions } from '@/utils';
+import whatToEatActions from '@/models/whatToEat/whatToEat.action';
+import whatToEatMode from '@/models/whatToEat/whatToEat.model';
+import { Food } from '@/types/Food.d';
 import Cover from './Cover';
 
 const { Search } = Input;
@@ -28,11 +28,17 @@ const ModeSwitch = styled.div`
 
 interface OwnProps {
   /**
-   * 初始模式
+   * 初始模式 -
    * 注意: 此处不能使用 Mode 只能这样写, 不然 docz 和 生成组件元数据时都不能正确的识别出类型
+   * @workflow - 此注解标识标识该prop, 需要工作流设计器配置
    * @default draw
    */
   defaultMode: 'draw' | 'search';
+  /**
+   * 模式 -
+   * 组件mount后, 可通过 mode 改变组件模式
+   */
+  mode: 'draw' | 'search';
 }
 
 interface StateProps {
@@ -46,8 +52,13 @@ interface DispatchProps {
 type Props = StateProps & DispatchProps & OwnProps;
 
 export const WhatToEat = (props: Props) => {
-  const { defaultMode, food, whatToEatBoundActions } = props;
+  const { defaultMode, food, whatToEatBoundActions, mode: propMode } = props;
+
   const [mode, setMode] = useState(defaultMode as Mode);
+
+  useEffect(() => {
+    if (propMode) setMode(propMode as Mode);
+  }, [propMode]);
 
   const switchMode = (checked: boolean) => {
     setMode(checked ? Mode.SEARCH : Mode.DRAW);
@@ -56,29 +67,27 @@ export const WhatToEat = (props: Props) => {
   const { draw, search } = whatToEatBoundActions;
 
   return (
-    <>
-      <FoodCard
-        actions={[
-          mode === Mode.DRAW ? (
-            <Button type="primary" block onClick={draw}>
-              Let me see
-            </Button>
-          ) : (
-            <Search onSearch={search} placeholder="输入关键字 - 回车" />
-          ),
-          <ModeSwitch>
-            手动搜索:{' '}
-            <Switch
-              defaultChecked={mode === Mode.SEARCH}
-              checked={mode === Mode.SEARCH}
-              onChange={switchMode}
-            />
-          </ModeSwitch>,
-        ]}
-      >
-        <Cover url={food.img} name={food.name} />
-      </FoodCard>
-    </>
+    <FoodCard
+      actions={[
+        mode === Mode.DRAW ? (
+          <Button type="primary" block onClick={draw}>
+            Let me see
+          </Button>
+        ) : (
+          <Search onSearch={search} placeholder="输入关键字 - 回车" />
+        ),
+        <ModeSwitch>
+          手动搜索:{' '}
+          <Switch
+            defaultChecked={mode === Mode.SEARCH}
+            checked={mode === Mode.SEARCH}
+            onChange={switchMode}
+          />
+        </ModeSwitch>,
+      ]}
+    >
+      <Cover url={food.img} name={food.name} />
+    </FoodCard>
   );
 };
 
