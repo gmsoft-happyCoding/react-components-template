6565cc437cc96d3ae0199878203fc0f9bb471136
angular-moon <xiaoxuss@163.com>
2019-04-28 14:35:28 +0800

    update antd@3.16.6-custom.0, remove use LocaleProvider
    antd定制版本已经替换默认语言为中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
 import React from 'react';
 import { hot, setConfig } from 'react-hot-loader';
 import { Provider } from 'react-redux';
@@ -9,13 +7,11 @@ import { Mode } from './enums/Mode';
 import { stateContainer } from './utils';
 
 const App = () => (
-  <LocaleProvider locale={zhCN}>
-    <Provider store={stateContainer._store}>
-      <Router history={stateContainer._history}>
-        <WhatToEat defaultMode={Mode.DRAW} />
-      </Router>
-    </Provider>
-  </LocaleProvider>
+  <Provider store={stateContainer._store}>
+    <Router history={stateContainer._history}>
+      <WhatToEat defaultMode={Mode.DRAW} />
+    </Router>
+  </Provider>
 );
 
 // @ts-ignore
