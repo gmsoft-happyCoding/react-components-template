442bbc510c84e32684a68cc2c97286764646fffa
angular-moon <xiaoxuss@163.com>
2020-07-01 14:48:00 +0800

    change easy-mock domain

--- .easy-mock.js
+++ .easy-mock.js
@@ -1,13 +1,13 @@
 module.exports = {
   // easy-mock 服务器
-  host: 'http://192.168.2.11:7300',
+  host: 'http://easy-mock.gm',
   // 生成文件输入目录
   output: 'src/api',
   // 生成代码使用的模板
   template: 'gmsoft-happyCoding/axios-ts',
   projects: [
     {
-      // easy-mock project id http://192.168.2.11:7300/project/5c514bf9cd2f550e9dfbb515
+      // easy-mock project id http://easy-mock.gm/project/5c514bf9cd2f550e9dfbb515
       id: '5c514bf9cd2f550e9dfbb515',
       name: 'whatToEat',
       // urlPreprocessor: url => url.replace('/demo-gateway', ''),
