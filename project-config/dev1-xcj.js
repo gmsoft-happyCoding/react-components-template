1609d4000158c411c8940c13c637f5651ad95ccd
angular-moon <xiaoxuss@163.com>
2021-11-15 19:42:16 +0800

    修改发布配置

--- project-config/dev1-xcj.js
+++ project-config/dev1-xcj.js
@@ -6,9 +6,7 @@ module.exports = {
     // 生成 sourceMap
     REACT_APP_GENERATE_SOURCEMAP: true,
     // 应用部署的域名和子路径
-    REACT_APP_PUBLIC_URL: '//registry.gmsofttest.com/{{projectName}}',
-    // 网关服务器base路径
-    REACT_APP_API_GATEWAY_BASE: '//www.test.com/demo/demo-gateway',
+    REACT_APP_PUBLIC_URL: '//registry.xcjdev1.gm/{{projectName}}',
     // 基础服务网关
     REACT_APP_DJC_GATEWAY_BASE: '//www.test.com/djc-gateway',
     // 发布部署, 建议配置不然每次都需要交互设置
@@ -18,8 +16,7 @@ module.exports = {
      * see: https://192.168.2.10:8080/svn/GovProEleTrade/安装与配置/部署配置/发布配置 or your .deployrc config info
      */
     REACT_APP_DEPLOY_MACHINES: [
-      { machine: 'machine1', where: [{ rootKey: 'nginx.webcomponent', path: '{{projectName}}' }] },
-      { machine: 'machine2', where: [{ rootKey: 'nginx.webcomponent', path: '{{projectName}}' }] },
+      { machine: 'machine01', where: [{ rootKey: 'nginx.webcomponent', path: '{{projectName}}' }] },
     ],
   },
 };
