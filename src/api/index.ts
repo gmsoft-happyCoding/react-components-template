24faad64d5e40810396311527f15381daef8ead4
angular-moon <xiaoxuss@163.com>
2022-12-04 21:43:24 +0800

    change setInterceptor load place

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,3 @@
-/* eslint-disable */
 import * as whatToEat from './whatToEat';
-import './setInterceptor';
 
 export { whatToEat };
--- src/api/setInterceptor.ts
+++ src/api/setInterceptor.ts
@@ -1,12 +1,14 @@
-import { useRequestInterceptor, useResponseInterceptor } from './util';
 import { showNetworkError } from '@/utils';
 import { axiosTokenInterceptor } from '@gmsoft/auth-sdk';
+import { AxiosInstance } from 'axios';
 
-useRequestInterceptor(axiosTokenInterceptor());
+export default (instance: AxiosInstance) => {
+  instance.interceptors.request.use(axiosTokenInterceptor());
 
-const errorHandler = error => {
-  showNetworkError(error);
-  return Promise.reject(error);
+  const errorHandler = error => {
+    showNetworkError(error);
+    return Promise.reject(error);
+  };
+
+  instance.interceptors.response.use(undefined, errorHandler);
 };
-
-useResponseInterceptor(undefined, errorHandler);
