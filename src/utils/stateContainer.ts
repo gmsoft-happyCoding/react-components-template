7c4c1d3f914b65a11d2680e4431867e8488624bb
angular-moon <xiaoxuss@gmail.com>
2024-03-12 14:27:00 +0800

    update state-container@1.6.0

--- src/utils/stateContainer.ts
+++ src/utils/stateContainer.ts
@@ -1,13 +1,19 @@
 import { create } from 'state-container';
 import history from './history';
 
+const origin =
+  process.env.NODE_ENV === 'development'
+    ? `https://${process.env['business.dev-plat-domain']}`
+    : undefined;
+
 const stateContainer = create({
   history,
   NODE_ENV: process.env.NODE_ENV,
   useGlobalContextPlugin: true,
   globalContextOpts: {
     djcGatewayBaseUrl: process.env['gateway.djc'],
-    notLoadMe: true,
+    origin,
+    meOrigin: origin,
   },
   onError: err => {
     // err.preventDefault();
